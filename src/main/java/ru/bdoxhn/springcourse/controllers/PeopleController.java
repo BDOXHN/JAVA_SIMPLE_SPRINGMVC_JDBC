1ff21ca5270eea69fb8a1a027ca860bf77c1e8f5
Mikhail Stanishevsky <bdoxhn.work@gmail.com>
2022-05-25 12:16:13 +0700

    commit_1

---
+++ src/main/java/ru/bdoxhn/springcourse/controllers/PeopleController.java
@@ -0,0 +1,76 @@
+package ru.bdoxhn.springcourse.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.bdoxhn.springcourse.dao.PersonDAO;
+import ru.bdoxhn.springcourse.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+
+    //@Autowired
+    //private PersonDAO personDAO;
+    //ниже то же самое, но внедрение через конструктор
+    private final PersonDAO personDAO;
+
+    @Autowired //можно писать, можно не писать, спринг сам внедрит
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        //Получим всех людей из DAO
+        //Передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        //Получим одного человека по его id из DAO
+        //Передадим этого человека на отображение в представление
+
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+//    public String newPerson(Model model){
+//        model.addAttribute("person", new Person());
+//        return "people/new";
+//    }
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+}
